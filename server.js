98fca05ae729c72f050c9735b393a5d0dfba99c1
rvssshashank <31001286+rvssshashank@users.noreply.github.com>
2017-08-23 11:07:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-
+  res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two', function (req, res) {
