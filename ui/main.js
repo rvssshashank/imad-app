5652cb42e0e5e8e97da7c7c9a63fe423e1c5b21d
rvssshashank <31001286+rvssshashank@users.noreply.github.com>
2017-08-23 13:22:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,5 +10,16 @@ element.innerHTML = 'New value';
 var img= document.getElementById('madi');
 
 img.onclick=function(){
+    v
     img.style.marginLeft = '100px';
 };
+var marginLeft= 0;
+// make img move like animation
+function moveRight(){
+    img.style.marginLeft= marginLeft+ 5;
+    marginLeft= marginLeft+ 'px';
+}
+img.onclick=function(){
+    var interval= setInterval(moveRight,100);
+};
+    
