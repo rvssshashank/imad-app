f2e0115d3f5a24756d345b909f9f3617b207e3ca
rvssshashank <31001286+rvssshashank@users.noreply.github.com>
2017-08-24 18:40:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ var htmlTemplate=
                     <div class=content>
                         <h3 class=heading>${heading}</h3>
                         <div>
-                         ${date.toDate()}
+                         ${date}
                         </div>
                         <div>
                         ${content}
