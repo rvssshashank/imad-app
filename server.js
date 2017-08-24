1a90b49ac4b0adda6d4a961aa3c43c12df91733a
rvssshashank <31001286+rvssshashank@users.noreply.github.com>
2017-08-24 18:38:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -112,7 +112,7 @@ app.get('articles/:articleName', function (req, res)
                 res.send(createTemplate(articleData));
             }
         }
-    })
+    });
   
   // articles[articlaName]= {}content objects for article-one
 });
