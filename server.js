693ce319811a1a811218ad06e1c2dccff82473c8
rvssshashank <31001286+rvssshashank@users.noreply.github.com>
2017-08-24 18:07:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -69,13 +69,15 @@ var htmlTemplate=
 return htmlTemplate;
 }
 
-app.get('/test_db',funtion(req,res){
+app.get('/test-db',function(req,res)
+{
     //make a select request
     //return a response with results
     pool.query('SELECT * FROM test', function(err, result)
     {if(err){
         res.status(500).send(err.toString());
-        }else{
+        }else
+        {
             res.send(JSON.stringify(result));
         }
     });
