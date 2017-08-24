496a7126545c86ed945fae17281f60da01ae32ad
rvssshashank <31001286+rvssshashank@users.noreply.github.com>
2017-08-24 18:09:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 
-var Pool= require(pg).Pool;
+var Pool= require('pg').Pool;
 var pool = new Pool({
   user: 'rshashi57r',
   host: 'db.imad.hasura-app.io',
