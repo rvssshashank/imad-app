984e3c802533f731ec7f3ed71c4c99c65f966cb1
rvssshashank <31001286+rvssshashank@users.noreply.github.com>
2017-08-31 13:59:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,4 +24,37 @@ function moveRight(){
 img.onclick=function(){
     var interval= setInterval(moveRight,50);
 };
-    
+
+
+// submit username and password
+   var submit= document.getElementById('submt_butn');
+   submit.onclick =function(){
+       
+       //create a request object
+       var request = new XMLHttpRequest();
+       
+       // Capture the response and store it in variable
+       request.onreadystatechange= function()
+     {
+         if(request.readyState === XMLHttpRequest.DONE){
+             //take some action
+             if(request.status === 200){
+                 alert('logged in success');
+             }
+             else if(request.status=== 403){
+                 alert('incorrect credentials');
+             }
+             else if(request.status===500){
+                 alert('Something went wrong');
+             }
+         }
+     };
+     
+     var username = document.getElementById('username');
+     var password = document.getElementById('password');
+     console.log(username);
+     console.log(password);
+     request.open('POST','http://rsahshi57.imad.hasura-app.io/login',true);
+     request.setRequestHeader('Content-Type','application/json');
+     request.send(JSON.stringify({username:username, password:password}));
+   };
