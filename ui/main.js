fccc41759a3494ec29a46b374c48be8ea56024cf
rvssshashank <31001286+rvssshashank@users.noreply.github.com>
2017-08-31 20:42:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -58,5 +58,5 @@ img.onclick=function(){
      console.log(password);
      request.open('POST','http://rshashi57.imad.hasura-app.io/login',true);
      request.setRequestHeader('Content-Type','application/json');
-     request.send(JSON.stringify({username:username, password:password}));
+     request.send(JSON.stringify({username: username, password: password}));
    };
